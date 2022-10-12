5ba03499709bc2deb37f640d6d10f2f361476ced
reizen-desu <reizen.dos.santos@gmail.com>
2022-10-12 23:36:49 +0200

    Install Popperjs & import bootstrap

--- src/main.js
+++ src/main.js
@@ -7,4 +7,8 @@ import './assets/styles/font-awesome.min.css'
 import './assets/styles/main.css'
 import './assets/styles/style.css'
 
+// We're using Bootstrap 5
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
 createApp(App).mount('#app')
