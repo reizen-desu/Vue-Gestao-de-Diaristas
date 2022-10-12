f55e558a0c347de90541d4e713f4451ae76aaf14
reizen-desu <reizen.dos.santos@gmail.com>
2022-10-12 23:29:46 +0200

    Import some CSS files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import externall bootstrap versions
+import './assets/styles/fontawesome.min.css'
+import './assets/styles/font-awesome.min.css'
+import './assets/styles/main.css'
+import './assets/styles/style.css'
+
 createApp(App).mount('#app')
