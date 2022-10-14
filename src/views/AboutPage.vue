f2b99ba5b502080b09a96d323397973c325c81e5
reizen-desu <reizen.dos.santos@gmail.com>
2022-10-14 02:17:28 +0200

    Add small description to About page

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h4>
+    O sistema tem como objectivo tornar fácil a procura de uma diarista, assim
+    economizando tempo das pessoas que procuram por uma diarista e criar oportunidades de
+    trabalho
+  </h4>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+h4 {
+  line-height: 1.2;
+  color: #212529;
+  text-align: center;
+  margin: 0;
+  padding: 40px;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
