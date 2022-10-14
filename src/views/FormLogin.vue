2c1341fc2742ffc2ea2feefcd22f5431a0370d98
reizen-desu <reizen.dos.santos@gmail.com>
2022-10-14 02:17:39 +0200

    Add login form code

---
+++ src/views/FormLogin.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="bg-light modal fade" id="login" aria-labelledby="modal-title">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modal-title">Login</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+
+        <div class="modal-body">
+          <p class="text-secondary">Preencha os campos abaixo para entrar</p>
+
+          <form id="loginForm" action="controller/loginController.php" method="POST">
+            <div class="form-floating mb-2">
+              <input
+                type="email"
+                name="email"
+                class="form-control"
+                id="email"
+                placeholder="nome@exemplo.com"
+                required
+              />
+              <label for="email">Email</label>
+            </div>
+            <div class="form-floating">
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
+              <label for="password">Senha</label>
+            </div>
+            <!-- <button type="submit" class="btn btn-primary">Entrar</button> -->
+          </form>
+        </div>
+        <!-- modal body -->
+        <div class="modal-footer">
+          <button class="btn btn-outline-secondary" data-bs-dismiss="modal">
+            Cancelar
+          </button>
+          <button type="submit" name="login" form="loginForm" class="btn btn-primary">
+            Entrar
+          </button>
+        </div>
+      </div>
+      <!-- modal content -->
+    </div>
+    <!-- modal dialog -->
+  </section>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
