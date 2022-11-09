fa0e5813f3728ae77ba0ec6a36ff07d87d196fb9
reizen-desu <nokiaq80@gmail.com>
2022-11-09 23:45:39 +0200

    feat: add a route for listing Diarists

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,37 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import LandingPage from '../views/LandingPage.vue';
-import AboutPage from '../views/AboutPage.vue';
-import NotFoundPage from '../views/NotFoundPage.vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import LandingPage from "../views/LandingPage.vue";
+import AboutPage from "../views/AboutPage.vue";
+import NotFoundPage from "../views/NotFoundPage.vue";
+import TelaVisitante from "../views/TelaVisitante.vue";
 
 // Configure Vue Router
 const routes = [
-    {
-        path: '/',
-        // name: 'home',
-        component: LandingPage
-    },
-    {
-        path: '/about',
-        // name: 'about',
-        component: AboutPage
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        // name: 'not-found',
-        component: NotFoundPage
-    }
-]
+	{
+		path: "/",
+		// name: 'home',
+		component: LandingPage,
+	},
+	{
+		path: "/about",
+		// name: 'about',
+		component: AboutPage,
+	},
+	{
+		path: "/:pathMatch(.*)*",
+		// name: 'not-found',
+		component: NotFoundPage,
+	},
+
+	{
+		path: "/diaristas",
+		name: "diaristas",
+		component: TelaVisitante,
+	},
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-})
+	history: createWebHashHistory(),
+	routes,
+});
 
-export default router
-
+export default router;
