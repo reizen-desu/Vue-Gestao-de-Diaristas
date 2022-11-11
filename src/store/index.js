99b1bff5a0e3b7f4bdcc14c65b5b3348654def35
reizen-desu <nokiaq80@gmail.com>
2022-11-11 09:31:10 +0200

    fix: remove unused code

--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,14 @@
-import axios from "axios";
-import router from "../router/index.js";
+import Vue from "vue";
 import Vuex from "vuex";
+import auth from "./auth";
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
-		authenticated: false,
-		user: null,
-		token: null,
+		count: 0,
 	},
-
-	mutations: {
-		authenticateUser(state, data) {
-			state.authenticated = true;
-			state.token = data.token;
-			state.user = data.user;
-		},
+	modules: {
+		auth,
 	},
-	actions: {
-		async login({ commit }, credentials) {
-			await axios.get("sanctum/csrf-cookie");
-			const res = await axios.post(
-				"http://127.0.0.1:8000/api/visitante/login",
-				credentials
-			);
-			commit("authenticateUser", res.data);
-			router.push("/");
-		},
-	},
-	getters: {
-		isAuthenticated(state) {
-			return state.authenticated;
-		},
-		user(state) {
-			return state.user;
-		},
-		userToken(state) {
-			return state.token;
-		},
-	},
-	modules: {},
 });
