a3c3d4a78aeb7b238e1e118cd6e3fb9eb2c60b0e
reizen-desu <nokiaq80@gmail.com>
2022-11-11 09:24:32 +0200

    package: add Vue Toastification

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 import App from "./App.vue";
 import router from "./router";
 // import axios from "axios";
@@ -25,8 +29,25 @@ axios.defaults.headers["Authorization"] =
 
 // Vue.use(Vuex);
 const pinia = createPinia();
+const options = {
+	timeout: 3000,
+	position: "bottom-left",
+	transition: "Vue-Toastification__fade",
+	closeOnClick: true,
+	pauseOnFocusLoss: true,
+	pauseOnHover: true,
+	draggable: true,
+	draggablePercent: 0.6,
+	showCloseButtonOnHover: false,
+	hideProgressBar: true,
+	closeButton: "button",
+	icon: true,
+	rtl: false,
+};
+
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
 app.use(VueSweetalert2);
+app.use(Toast, options);
 app.mount("#app");
