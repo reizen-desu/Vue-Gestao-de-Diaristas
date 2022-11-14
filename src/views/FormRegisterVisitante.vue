5822e9d9b20089d6c2d688cb9e439d7de0f2fb6f
reizen-desu <nokiaq80@gmail.com>
2022-11-14 11:59:11 +0200

    feat: add register form for visitante

--- src/views/FormRegisterVisitante.vue
+++ src/views/FormRegisterVisitante.vue
@@ -4,11 +4,12 @@
     poderá publicar seus serviços. Preencha os campos abaixo para se registrar.
   </div>
 
-  <form action="controller/registerController.php" method="POST">
+  <form @submit.prevent="register">
     <div class="row">
       <div class="form-group col">
         <div class="form-floating">
-          <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome" required />
+          <input type="text" class="form-control" name="nome" v-model="visitante.nome" id="nome" placeholder="Nome"
+            required />
           <label for="nome" class="form-label">Nome</label>
         </div>
         <!-- <input type="text" class="form-control" id="nome" placeholder="Introduza seu nome" pattern="[^0-9]+" required> -->
@@ -16,7 +17,8 @@
 
       <div class="form-group col">
         <div class="form-floating">
-          <input type="text" class="form-control" id="apelido" name="apelido" placeholder="Apelido" required />
+          <input type="text" v-model="visitante.apelido" class="form-control" id="apelido" name="apelido"
+            placeholder="Apelido" required />
           <label for="apelido" class="form-label">Apelido</label>
         </div>
         <!-- <input type="text" class="form-control" id="apelido" placeholder="Introduza seu apelido" pattern="[^0-9]+" required> -->
@@ -26,8 +28,8 @@
     <br />
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="email_visitante" name="email" placeholder="Introduza seu email"
-        required />
+      <input type="email" v-model="visitante.email" class="form-control" id="email_visitante" name="email"
+        placeholder="Introduza seu email" required />
       <label for="email" class="form-label">Email</label>
     </div>
 
@@ -35,8 +37,8 @@
     <div class="row">
       <div class="form-group col">
         <label for="password_visitante">Senha</label>
-        <input type="password" class="form-control" id="password_visitante" name="password_visitante"
-          placeholder="Senha" onchange="validarSenhaVisitante();" required />
+        <input type="password" v-model="visitante.senha" class="form-control" id="password_visitante"
+          name="password_visitante" placeholder="Senha" onchange="validarSenhaVisitante();" required />
       </div>
 
       <div class="form-group col">
@@ -50,12 +52,14 @@
 
     <div class="form-group">
       <label for="nif">Contacto</label>
-      <input type="digit" class="form-control" id="contacto" name="contacto" placeholder="84 0000 000" required />
+      <input type="digit" v-model="visitante.telefone" class="form-control" id="contacto" name="contacto"
+        placeholder="84 0000 000" required />
     </div>
 
     <div class="form-group">
       <label for="morada">Morada</label>
-      <input type="text" class="form-control" id="morada" name="morada" placeholder="Introduza o seu bairro" />
+      <input type="text" v-model="visitante.morada" class="form-control" id="morada" name="morada"
+        placeholder="Introduza o seu bairro" />
     </div>
 
     <hr />
@@ -67,7 +71,41 @@
   </form>
 </template>
 <script>
-export default {};
+import { useToast } from 'vue-toastification';
+import axios from 'axios';
+
+export default {
+  name: "RegistarVisitante",
+  data() {
+    return {
+      visitante: {
+        nome: '',
+        apelido: '',
+        email: '',
+        senha: '',
+        telefone: '',
+        morada: '',
+      },
+    };
+  },
+
+  methods: {
+    register() {
+      const toast = useToast();
+      axios.post('/visitantes/register', this.visitante)
+        .then((response) => {
+          toast.success('Visitante registado com sucesso!');
+          // this.$router.push('/login');
+          console.log(response);
+        })
+        .catch((error) => {
+          toast.error('Erro ao registar visitante!');
+          console.log(error);
+        });
+    },
+  }
+
+};
 </script>
 <style>
 
